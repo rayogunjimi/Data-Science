4ef1040654e8301dd519377b1fb3985c5a9ba684
Ray Ogunjimi <81774312+rayogunjimi@users.noreply.github.com>
2022-04-23 14:08:33 -0400

    Add files via upload

---
+++ ogunjimi_homework1.R
@@ -0,0 +1,24 @@
+# Raymond Ogunjimi
+# Data Science
+# Homework 1
+# 18 January 2022
+
+sqrt(100489)
+
+vowel = 2 # 2 vowels in raymond
+consonant = 5 # 5 consonants in raymond
+vowel/consonant
+
+pi == 22/7
+
+sqrt(150) > 12
+sqrt(150) < 13
+
+install.packages("gapminder")
+library(gapminder)
+gapminder
+
+head(gapminder,6)
+# Variables are: country, continent, year, lifeExp, pop, gdpPercap
+
+tail(gapminder,10)
